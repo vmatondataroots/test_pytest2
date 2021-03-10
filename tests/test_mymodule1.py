82799a956cddbeb38adf78e8750acb13f68c8d1f
vmatondataroots <victor@dataroots.io>
2021-03-10 15:50:52 +0100

    finished git comments

--- tests/test_mymodule1.py
+++ tests/test_mymodule1.py
@@ -33,3 +33,16 @@ def test_myfunc1(a, expected):
 def test_myfunc2(get_data):
     test_len = mym1.myfunc2(get_data)
     assert test_len == len(range(5))
+
+# git init
+# git status
+# git branch my_branch
+# git branch -v
+# git checkout my_branch
+# git add .
+# git commit -m "expain commmit"
+# git push origin my_branch
+
+# git checkout main
+# git merge my_branch
+# git remote show origin
